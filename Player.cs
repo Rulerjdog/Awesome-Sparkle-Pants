072656462ac0bb0024092f0b9cae684f306f1a8d
daragonh <1daragonh12@gmail.com>
2017-05-09 17:39:01 -0400

    Add files via upload
    player needs to be a 2d physics as does the bullet(shovels) shovels needs to be attached to the player sprite

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    public int HP = 100;
+    public int playerspeed = 15;
+    public int jumps = 10;
+    //public Text HPtext;
+    public bool facingleft = false;
+    public bool facingright = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-Vector3.right * playerspeed * Time.deltaTime);
+            facingleft = true;
+            facingright = false;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(-Vector3.left * playerspeed * Time.deltaTime);
+            facingleft = false;
+            facingright = true;
+
+        }
+        if (Input.GetKey("space"))
+        {
+            transform.Translate(Vector3.up * jumps * Time.deltaTime);
+        }
+	}
+}
---
+++ shovels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shovels : MonoBehaviour
+{
+    public float shovelspeed = 40;
+
+    public Rigidbody2D shovelsob;
+
+    // Use this for initialization
+    void Fire()
+    {
+        Rigidbody2D shovelsobClone = (Rigidbody2D)Instantiate(shovelsob, transform.position, transform.rotation);
+        if (GetComponent<Player>().facingleft)
+        {
+            shovelsobClone.velocity = -transform.right * shovelspeed;
+        }
+        else if (GetComponent<Player>().facingright)
+        {
+            shovelsobClone.velocity = transform.right * shovelspeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("h"))
+        {
+            Fire();
+
+        }
+
+    }
+}
